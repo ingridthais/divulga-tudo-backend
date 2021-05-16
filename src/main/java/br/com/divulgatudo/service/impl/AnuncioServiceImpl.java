a3c508c99614f8a96100da405d2b3b645534bec4
Ingrid Thays <ingridthais1993@gmail.com>
2021-05-16 19:29:12 -0300

    criado a implementação do service

--- src/main/java/br/com/divulgatudo/service/impl/AnuncioServiceImpl.java
+++ src/main/java/br/com/divulgatudo/service/impl/AnuncioServiceImpl.java
@@ -2,42 +2,50 @@ package br.com.divulgatudo.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.divulgatudo.error.ResourceNotContentException;
 import br.com.divulgatudo.model.Anuncio;
+import br.com.divulgatudo.repository.AnuncioRepository;
 import br.com.divulgatudo.service.AnuncioService;
 
 @Service
 public class AnuncioServiceImpl implements AnuncioService {
 
+	@Autowired
+	AnuncioRepository repository;
+
 	@Override
 	public Anuncio create(Anuncio a) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(a);
 	}
 
 	@Override
 	public Anuncio update(Anuncio a, Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!repository.existsById(id)) {
+			throw new ResourceNotContentException("O Anuncio não existe!");
+		}
+		a.setId(id);
+		return repository.save(a);
 	}
 
 	@Override
-	public Anuncio findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Anuncio findById(Long id) {		
+		return repository.findById(id).orElseThrow(() -> new ResourceNotContentException("O Anuncio não existe!"));
 	}
 
 	@Override
-	public List<Anuncio> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Anuncio> findAll() {		
+		return repository.findAll();
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
-
+		if (!repository.existsById(id)) {
+			throw new ResourceNotContentException("O Anuncio não existe ou já foi excluido!");
+		}
+		repository.deleteById(id);
 	}
 
 }
