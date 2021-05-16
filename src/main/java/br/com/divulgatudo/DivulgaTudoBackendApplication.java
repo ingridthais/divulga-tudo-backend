ebc91844ba2d88ddbb0e187e8fb2a2bc8e92523e
Ingrid Thays <ingridthais1993@gmail.com>
2021-05-16 18:42:05 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/divulgatudo/DivulgaTudoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.divulgatudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivulgaTudoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivulgaTudoBackendApplication.class, args);
+	}
+
+}
